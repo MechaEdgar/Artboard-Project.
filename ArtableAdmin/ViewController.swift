513698fec2ee69ec8a82f88985be4aa0c774f859
Edgar <ee1206@hotmail.com>
2019-06-12 21:24:54 -0500

    ArtableAdmin target created.

---
+++ ArtableAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Edgar Escorza on 6/12/19.
+//  Copyright © 2019 Edgar Escorza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
