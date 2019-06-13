e051f4c5f9ab670dbe16dc01f725c681f27eb5b1
Edgar <ee1206@hotmail.com>
2019-06-12 21:02:10 -0500

    Initial Commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Edgar Escorza on 6/12/19.
+//  Copyright © 2019 Edgar Escorza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
