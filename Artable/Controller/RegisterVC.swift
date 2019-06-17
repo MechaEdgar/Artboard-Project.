5d52f2b9f89f70c6f49ba42658d19a5366895b95
Edgar <ee1206@hotmail.com>
2019-06-17 01:27:17 -0500

    Password Matching and Custom Views

--- Artable/Controller/RegisterVC.swift
+++ Artable/Controller/RegisterVC.swift
@@ -16,22 +16,52 @@ class RegisterVC: UIViewController {
     @IBOutlet weak var passwordTxt: UITextField!
     @IBOutlet weak var confirmPassTxt: UITextField!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var passImgCheck: UIImageView!
+    @IBOutlet weak var confirmImgCheck: UIImageView!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        passwordTxt.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+        confirmPassTxt.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func textFieldDidChange(_ textfield: UITextField) {
+        guard let passTxt = passwordTxt.text else { return }
+        
+        // If we have started typing in the confirm pass textfield.
+        if textfield == confirmPassTxt {
+            passImgCheck.isHidden = false
+            confirmImgCheck.isHidden = false
+        } else {
+            if passTxt.isEmpty {
+                passImgCheck.isHidden = true
+                confirmImgCheck.isHidden = true
+                confirmPassTxt.text = ""
+            }
+        }
+        
+        //Make it so when the passwords match, the checkmars turn green.
+        if passwordTxt.text == confirmPassTxt.text {
+            passImgCheck.image = UIImage(named: "green_check")
+            confirmImgCheck.image = UIImage(named: "green_check")
+        } else {
+            passImgCheck.image = UIImage(named: "red_check")
+            confirmImgCheck.image = UIImage(named: "red_check")
+        }
     }
     
     @IBAction func resgisterClicked(_ sender: Any) {
         guard let email = emailTxt.text ,
             let username = usernameTXT.text ,
             let password = passwordTxt.text,
-            !email.isEmpty,
-            !username.isEmpty,
-            !password.isEmpty else { return }
+            email.isNotEmpty,
+            username.isNotEmpty,
+            password.isNotEmpty else { return }
+        
+        activityIndicator.startAnimating()
         
         Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
             
@@ -39,6 +69,7 @@ class RegisterVC: UIViewController {
                 debugPrint(error)
                 return
             }
+            self.activityIndicator.stopAnimating()
             print("succesfully resgistered new user")
             
         }
---
+++ Shared/New Group/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  Artable
+//
+//  Created by Edgar Escorza on 6/15/19.
+//  Copyright © 2019 Edgar Escorza. All rights reserved.
+//
+
+import Foundation
+
+
+extension String {
+    
+    var isNotEmpty : Bool{
+        return !isEmpty
+    }
+    
+}
